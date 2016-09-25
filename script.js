c03535ccf507e8788fe6b0183306069270e91c8e
leeadrian841 <leeadrian841@gmail.com>
2016-09-25 22:12:34 +0800

    updated

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener()
+
 document.getElementById('1')
 document.getElementById('2')
 document.getElementById('3')
@@ -7,3 +9,6 @@ document.getElementById('6')
 document.getElementById('7')
 document.getElementById('8')
 document.getElementById('9')
+
+document.addEventListener('mouseover',#1)
+document.addEventListener('click',#1)
